7d40d348c1dc5e857083384a24f790e55ee8979d
Maryambghm2 <maryambghm2@outlook.fr>
2025-08-20 11:23:01 +0200

    init

---
+++ src/main/java/org/example/projetfinaltournois/ProjetFinalTournoisApplication.java
@@ -0,0 +1,13 @@
+package org.example.projetfinaltournois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFinalTournoisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetFinalTournoisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projetfinaltournois/ProjetFinalTournoisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projetfinaltournois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFinalTournoisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
